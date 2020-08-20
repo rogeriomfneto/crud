567d8b1301d128bb1ad4d2b539802d80a84a6a2b
Rogerio Neto <rogerioneto@hotmail.com>
2020-08-19 22:24:27 -0300

    project done

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,11 +1,13 @@
-import './Logo.css'
-import logo from '../../assets/imgs/logo.png'
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Logo.css';
+import logo from '../../assets/imgs/logo.png';
 
 export default (props) => (
   <aside className="logo">
-    <a href="/" className="logo">
+    <Link to="/" className="logo">
       <img src={logo} alt="logo" />
-    </a>
+    </Link>
   </aside>
 );
--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,16 +1,17 @@
-import './Nav.css'
-import React from 'react'
+import './Nav.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default (props) => (
   <aside className="menu-area">
     <nav className="menu">
       {/* Refatorar links para componentes (NavItem)*/}
-      <a href="#/">
+      <Link to="/">
         <i className="fa fa-home"></i> Início
-      </a>
-      <a href="#/users">
+      </Link>
+      <Link to="/users">
         <i className="fa fa-users"></i> Usuários
-      </a>
+      </Link>
     </nav>
   </aside>
 );
